b93142a869dfcdc432b50fdc0d6345eb1ae2a40e
Ali Maher <ali.maher.mohammed@gmail.com>
2022-12-09 07:55:26 +0200

    Addig new items

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const { ApolloServer } = require('apollo-server')
 const { typeDefs } = require("./schema")
-const { resolvers } = require("./resolvers")
+const { Query } = require("./resolvers/Query")
+const { Mutation } = require("./resolvers/Mutation")
+const { Product } = require("./resolvers/Product")
+const { Category } = require("./resolvers/Category")
 const { products, categories, reviews } = require("./db")
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolvers: {
+    Query,
+    Mutation,
+    Product,
+    Category
+  },
   context: {
     products,
     categories,
--- resolvers/index.js
+++
@@ -1,8 +0,0 @@
-const { Query } = require("./Query")
-const { Product } = require("./Product")
-const { Category } = require("./Category")
-exports.resolvers = {
-  Query,
-  Product,
-  Category
-}
